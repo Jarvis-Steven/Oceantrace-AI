3ca68f3ba7074edf92c8202a58b42d383cbbe624
Jarvis-Steven <jarvissteven413@gmail.com>
2026-09-04 08:33:50 +0530

    fix: switch MapView basemap tiles to Esri World Gray Canvas tile service

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -574,11 +574,11 @@ function MapViewContent() {
               <ZoomControl position="bottomleft" />
 
               <TileLayer
-                attribution='&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                attribution='&copy; <a href="https://www.esri.com/">Esri</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url={
                   theme === "dark"
-                    ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                    : "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    ? "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                    : "https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
                 }
                 maxZoom={16}
                 noWrap={true}
